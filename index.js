f1e450c1a627764fbac573d5ead59e8f004bb974
Chamunorwa Mutezva <ckmutezva@gmail.com>
2021-04-11 23:11:41 +0200

    feat: parceljs setup

---
+++ index.js
@@ -0,0 +1,16 @@
+//Intersection Observer api, 
+const image__list = document.querySelectorAll(".image__item");
+console.log(image__list)
+observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.classList.add('.slideInLeft');
+        } else {
+            entry.target.classList.remove('.slideInLeft');
+        }
+    });    
+});
+
+image__list.forEach(heads => {
+    observer.observe(heads);
+});
